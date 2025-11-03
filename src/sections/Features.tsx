03380550ba2ff5086b4cbe36a7f2e09a33379149
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-11-03 19:46:41 +0530

    added featureCrad component

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,10 @@
+import Tag from "@/components/Tag";
+import FeatureCard from "@/components/FeatureCard";
+import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg'
+import avatar2 from '@/assets/images/avatar-lula-meyers.jpg'
+import avatar3 from '@/assets/images/avatar-florence-shaw.jpg'
+import Image from "next/image";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +16,38 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return <section className="py-24">
+        <div className="container">
+            <div className="flex justify-center"> 
+            <Tag> Features</Tag>
+            </div>
+            <h2 className="text-6xl font-medium text-center mt-6"> Where power meets <span className="text-lime-400"> simplicity</span>
+            </h2>
+            <div className="mt-12 grid  grid-cols-1 gap-8 ">
+                <FeatureCard title="real-time Collaboration" description=" Work together seamlessly with conflictionediting.">
+                <div>
+                    <div>
+                        <Image src={avatar1} alt='Avatar 1'/>
+                    </div>
+                    <div>
+                        <Image src={avatar2} alt='Avatar 2'/>
+                    </div>
+                    <div>
+                        <Image src={avatar3} alt='Avatar 3'/>
+                    </div>
+                </div>
+                </FeatureCard>
+               <FeatureCard title="Interactive Prototyping" description=" Enagage your client with prototypes that require user actions."></FeatureCard>
+               <FeatureCard title="Keyboard Wuick Actions" description=" Powerful commands to help you create designs more quickly."></FeatureCard>
+            </div>
+            <div>
+                { features.map(feature => (
+                    <div key={feature}>
+                        <span></span>
+                        <span>{feature}</span>
+                        </div>
+                ))}
+            </div>
+        </div>
+    </section>;
 }
